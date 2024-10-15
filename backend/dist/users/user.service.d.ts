efdc549cd3f476c33d53ead8299f87174f472c2b
codechan-dev <89308654+codechan-dev@users.noreply.github.com>
2024-10-15 07:50:33 +0530

    Delete backend directory

--- backend/dist/users/user.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-export declare class UsersService {
-    private usersRepository;
-    constructor(usersRepository: Repository<User>);
-    findOne(username: string): Promise<User | undefined>;
-    findAll(): Promise<User[]>;
-    create(user: Partial<User>): Promise<User>;
-}
--- backend/dist/users/users.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersService } from './user.service';
-export declare class UsersController {
-    private usersService;
-    constructor(usersService: UsersService);
-    getUsers(): Promise<{
-        id: number;
-        username: string;
-    }[]>;
-}
